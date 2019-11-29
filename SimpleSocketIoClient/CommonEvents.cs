81ab8a6ccf1b510db24285b0526a774065c244f0
Konstantin S <havendv@gmail.com>
2019-11-29 16:04:31 +1000

    Added CommonEvents

---
+++ SimpleSocketIoClient/CommonEvents.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleSocketIoClient
+{
+    /// <summary>
+    /// Common events like "message"
+    /// </summary>
+    public static class CommonEvents
+    {
+        /// <summary>
+        /// Message event
+        /// </summary>
+        public const string Message = "message";
+    }
+}

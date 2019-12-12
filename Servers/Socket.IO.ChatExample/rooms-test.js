287e7d54db1f55c3d07dc0a8fc6e49b815d5645b
Konstantin S <havendv@gmail.com>
2019-12-13 03:50:59 +1000

    Fixed js style

--- Servers/Socket.IO.ChatExample/rooms-test.js
+++ Servers/Socket.IO.ChatExample/rooms-test.js
@@ -7,7 +7,7 @@ var io = require('socket.io')(server);
 var port = 1465;
 
 server.listen(port, () => {
-  console.log('Server listening at port %d', port);
+    console.log('Server listening at port %d', port);
 });
 
 // Routing
@@ -18,60 +18,62 @@ app.use(express.static(path.join(__dirname, 'public')));
 var numUsers = 0;
 
 io.on('connection', (socket) => {
-  var addedUser = false;
-  socket.join('some room');
+    var addedUser = false;
 
-  // when the client emits 'new message', this listens and executes
-  socket.on('new message', (data) => {
-    // we tell the client to execute 'new message'
-    io.to('some room').emit('new message', {
-      username: socket.username,
-      message: data
+    // connect to room
+    socket.join('some room');
+
+    // when the client emits 'new message', this listens and executes
+    socket.on('new message', (data) => {
+        // we tell the client to execute 'new message'
+        io.to('some room').emit('new message', {
+            username: socket.username,
+            message: data
+        });
     });
-  });
 
-  // when the client emits 'add user', this listens and executes
-  socket.on('add user', (username) => {
-    if (addedUser) return;
+    // when the client emits 'add user', this listens and executes
+    socket.on('add user', (username) => {
+        if (addedUser) return;
 
-    // we store the username in the socket session for this client
-    socket.username = username;
-    ++numUsers;
-    addedUser = true;
-    socket.emit('login', {
-      numUsers: numUsers
+        // we store the username in the socket session for this client
+        socket.username = username;
+        ++numUsers;
+        addedUser = true;
+        socket.emit('login', {
+            numUsers: numUsers
+        });
+        // echo globally (all clients) that a person has connected
+        io.to('some room').emit('user joined', {
+            username: socket.username,
+            numUsers: numUsers
+        });
     });
-    // echo globally (all clients) that a person has connected
-    io.to('some room').emit('user joined', {
-      username: socket.username,
-      numUsers: numUsers
+
+    // when the client emits 'typing', we broadcast it to others
+    socket.on('typing', () => {
+        io.to('some room').emit('typing', {
+            username: socket.username
+        });
     });
-  });
 
-  // when the client emits 'typing', we broadcast it to others
-  socket.on('typing', () => {
-	io.to('some room').emit('typing', {
-      username: socket.username
+    // when the client emits 'stop typing', we broadcast it to others
+    socket.on('stop typing', () => {
+        io.to('some room').emit('stop typing', {
+            username: socket.username
+        });
     });
-  });
 
-  // when the client emits 'stop typing', we broadcast it to others
-  socket.on('stop typing', () => {
-	io.to('some room').emit('stop typing', {
-      username: socket.username
+    // when the user disconnects.. perform this
+    socket.on('disconnect', () => {
+        if (addedUser) {
+            --numUsers;
+
+            // echo globally that this client has left
+            io.to('some room').emit('user left', {
+                username: socket.username,
+                numUsers: numUsers
+            });
+        }
     });
-  });
-
-  // when the user disconnects.. perform this
-  socket.on('disconnect', () => {
-    if (addedUser) {
-      --numUsers;
-
-      // echo globally that this client has left
-      io.to('some room').emit('user left', {
-        username: socket.username,
-        numUsers: numUsers
-      });
-    }
-  });
 });

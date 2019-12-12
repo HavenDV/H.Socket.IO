f01199ca771b4868a75b5d92e0970f3374272713
Konstantin S <havendv@gmail.com>
2019-12-13 03:26:08 +1000

    Added SocketIo.Debug test

--- SimpleSocketIoClient.IntegrationTests/SocketIoClientTests.cs
+++ SimpleSocketIoClient.IntegrationTests/SocketIoClientTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net.WebSockets;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -9,6 +10,8 @@ namespace SimpleSocketIoClient.IntegrationTests
     [TestClass]
     public class SocketIoClientTests
     {
+        private const string LocalCharServerUrl = "ws://localhost:1465/";  
+
         // ReSharper disable once ClassNeverInstantiated.Local
         // ReSharper disable UnusedAutoPropertyAccessor.Local
         private class ChatMessage
@@ -18,7 +21,7 @@ namespace SimpleSocketIoClient.IntegrationTests
             public long NumUsers { get; set; }
         }
 
-        private static async Task ConnectToChatNowShBaseTest(string url)
+        private static async Task ConnectToChatBaseTest(string url)
         {
             using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(10));
 
@@ -119,13 +122,77 @@ namespace SimpleSocketIoClient.IntegrationTests
         [TestMethod]
         public async Task ConnectToChatNowShTest()
         {
-            await ConnectToChatNowShBaseTest("wss://socket-io-chat.now.sh/");
+            await ConnectToChatBaseTest("wss://socket-io-chat.now.sh/");
         }
 
         [TestMethod]
         public async Task ConnectToLocalChatServerTest()
         {
-            await ConnectToChatNowShBaseTest("ws://localhost:1465/");
+            try
+            {
+                await ConnectToChatBaseTest(LocalCharServerUrl);
+            }
+            catch (WebSocketException exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+
+        [TestMethod]
+        public async Task ConnectToLocalChatServerDebugTest()
+        {
+            try
+            {
+                using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+                var cancellationToken = cancellationTokenSource.Token;
+
+#if NETCOREAPP3_0 || NETCOREAPP3_1
+                await using var client = new SocketIoClient();
+#else
+                using var client = new SocketIoClient();
+#endif
+
+                client.Connected += (sender, args) => Console.WriteLine("Connected");
+                client.Disconnected += (sender, args) => Console.WriteLine($"Disconnected. Reason: {args.Reason}, Status: {args.Status:G}");
+                client.AfterEvent += (sender, args) => Console.WriteLine($"AfterEvent: {args.Value}");
+                client.AfterUnhandledEvent += (sender, args) => Console.WriteLine($"AfterUnhandledEvent: {args.Value}");
+                client.AfterException += (sender, args) => Console.WriteLine($"AfterException: {args.Value}");
+
+                client.EngineIoClient.Opened += (sender, args) => Console.WriteLine("EngineIoClient.Opened");
+                client.EngineIoClient.Closed += (sender, args) => Console.WriteLine($"EngineIoClient.Closed. Reason: {args.Reason}, Status: {args.Status:G}");
+                client.EngineIoClient.Upgraded += (sender, args) => Console.WriteLine($"EngineIoClient.Upgraded: {args.Value}");
+                client.EngineIoClient.AfterException += (sender, args) => Console.WriteLine($"EngineIoClient.AfterException: {args.Value}");
+                client.EngineIoClient.AfterMessage += (sender, args) => Console.WriteLine($"EngineIoClient.AfterMessage: {args.Value}");
+                client.EngineIoClient.AfterNoop += (sender, args) => Console.WriteLine($"EngineIoClient.AfterNoop: {args.Value}");
+                client.EngineIoClient.AfterPing += (sender, args) => Console.WriteLine($"EngineIoClient.AfterPing: {args.Value}");
+                client.EngineIoClient.AfterPong += (sender, args) => Console.WriteLine($"EngineIoClient.AfterPong: {args.Value}");
+
+                client.EngineIoClient.WebSocketClient.Connected += (sender, args) => Console.WriteLine("WebSocketClient.Connected");
+                client.EngineIoClient.WebSocketClient.Disconnected += (sender, args) => Console.WriteLine($"WebSocketClient.Disconnected. Reason: {args.Reason}, Status: {args.Status:G}");
+                client.EngineIoClient.WebSocketClient.AfterText += (sender, args) => Console.WriteLine($"WebSocketClient.AfterText: {args.Value}");
+                client.EngineIoClient.WebSocketClient.AfterException += (sender, args) => Console.WriteLine($"WebSocketClient.AfterException: {args.Value}");
+                client.EngineIoClient.WebSocketClient.AfterBinary += (sender, args) => Console.WriteLine($"WebSocketClient.AfterBinary: {args.Value.Length}");
+
+                await client.ConnectAsync(new Uri(LocalCharServerUrl), cancellationToken);
+                await client.Emit("add user", "C# SimpleSocketIoClient Test User", cancellationToken);
+                await Task.Delay(TimeSpan.FromMilliseconds(200), cancellationToken);
+                await client.Emit("typing", cancellationToken);
+                await Task.Delay(TimeSpan.FromMilliseconds(200), cancellationToken);
+                await client.Emit("new message", "hello", cancellationToken);
+                await Task.Delay(TimeSpan.FromMilliseconds(200), cancellationToken);
+                await client.Emit("stop typing", cancellationToken);
+                await Task.Delay(TimeSpan.FromSeconds(2), cancellationToken);
+                await client.DisconnectAsync(cancellationToken);
+
+                Console.WriteLine();
+                Console.WriteLine($"WebSocket State: {client.EngineIoClient.WebSocketClient.Socket.State}");
+                Console.WriteLine($"WebSocket CloseStatus: {client.EngineIoClient.WebSocketClient.Socket.CloseStatus}");
+                Console.WriteLine($"WebSocket CloseStatusDescription: {client.EngineIoClient.WebSocketClient.Socket.CloseStatusDescription}");
+            }
+            catch (WebSocketException exception)
+            {
+                Console.WriteLine(exception);
+            }
         }
     }
 }

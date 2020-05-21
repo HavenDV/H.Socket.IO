bdf1775d9829ce5dbc79d22e7403481a6a00dc6f
Konstantin S <havendv@gmail.com>
2020-05-21 23:53:30 +1000

    Refactored test servers

--- src/TestServers/Socket.IO.ChatExample/namespaces.js
+++ src/TestServers/Socket.IO.ChatExample/namespaces.js
@@ -7,14 +7,9 @@ server.listen(port, () => {
 	console.log("Server listening at port %d", port);
 });
 
-app.get("/", function (req, res) {
-	res.sendfile("index.html");
-});
-
 var nsp = io.of("/my");
 nsp.on("connection", function (socket) {
 	console.log("someone connected");
-	//nsp.emit("hi", "Hello everyone!");
 
 	socket.on("message", (data) => {
 		console.log(`new message: ${data}`);
--- src/TestServers/Socket.IO.ChatExample/rooms.js
+++ src/TestServers/Socket.IO.ChatExample/rooms.js
@@ -12,56 +12,46 @@ var numUsers = 0;
 io.on("connection", (socket) => {
     var addedUser = false;
 
-    // connect to room
     socket.join("some room");
 
-    // when the client emits 'new message', this listens and executes
     socket.on("new message", (data) => {
-        // we tell the client to execute 'new message'
         io.to("some room").emit("new message", {
             username: socket.username,
             message: data
         });
     });
 
-    // when the client emits 'add user', this listens and executes
     socket.on("add user", (username) => {
         if (addedUser) return;
 
-        // we store the username in the socket session for this client
         socket.username = username;
         ++numUsers;
         addedUser = true;
         socket.emit("login", {
             numUsers: numUsers
         });
-        // echo globally (all clients) that a person has connected
         io.to("some room").emit("user joined", {
             username: socket.username,
             numUsers: numUsers
         });
     });
 
-    // when the client emits 'typing', we broadcast it to others
     socket.on("typing", () => {
         io.to("some room").emit("typing", {
             username: socket.username
         });
     });
 
-    // when the client emits 'stop typing', we broadcast it to others
     socket.on("stop typing", () => {
         io.to("some room").emit("stop typing", {
             username: socket.username
         });
     });
 
-    // when the user disconnects.. perform this
     socket.on("disconnect", () => {
         if (addedUser) {
             --numUsers;
 
-            // echo globally that this client has left
             io.to("some room").emit("user left", {
                 username: socket.username,
                 numUsers: numUsers
